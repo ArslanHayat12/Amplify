8c56b42f472278427e24dde8407c7e049111a52f
aneesanwar308 <anwaranees250@gmail.com>
2022-02-23 08:34:27 +0500

    setting admin side url

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,7 @@ export const getEmbededURL = (url, user, isAdmin, isBusiness) => {
     const filter = isAdmin ? '' : isBusiness ? `business.keyword:${businessId}` : `practitioner.keyword:${practitionerId}`
 
     const key = isAdmin ? '' : isBusiness ? `business.keyword` : `practitioner.keyword`
-    return generatedUrl(url, filter, key)
+    return generatedUrl(url, filter, key, isAdmin)
 
 }
 
@@ -22,8 +22,11 @@ export const getCustomRoleType = (customRole) => {
     return { isBusiness, isPractitioner, isAdmin }
 }
 
-export const generatedUrl = (url, filter, key) => {
+export const generatedUrl = (url, filter, key, isAdmin) => {
 
+    if (isAdmin) {
+        return url && `${url.url + url.dashboardId}?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15d%2Cto%3Anow))`
+    }
     return url && `${url.url + url.dashboardId}?embed=true&_g=(filters:!((meta:(index:'90bf3f10-5e50-11ec-b275-070a29d78d3f',key:${key},negate:!f,type:phrase),
 query:(match_phrase:(${filter}))))%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-4y%2Cto%3Anow))&show-time-filter=true&hide-filter-bar=true`
 
